535100a314f6d85e43aedbab412fde7d56be31e1
Marlon <marlonmontenegropaz@gmail.com>
2025-02-19 14:38:41 -0600

    Add project files.

---
+++ Producto.cs
@@ -0,0 +1,20 @@
+﻿namespace Bl
+{
+    public class Producto
+    {
+
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public decimal Precio { get; set; }
+        public float Cantidad { get; set; }
+        public decimal valor { get { return Precio * (decimal) Cantidad; } }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Venta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bl
+{
+    public class Venta
+    {
+
+        private string cliente;
+
+        private DateTime fecha;
+
+        private List<Producto> productos;
+
+        public List<Producto> Productos
+        {
+            get { return productos; }
+        }
+
+        public Venta(string cliente)
+        {
+            this.cliente = cliente;
+            this.fecha = DateTime.Now;
+            this.productos = new List<Producto>();
+        }
+
+       
+
+        public void AgregarProducto(Producto producto)
+        {
+            productos.Add(producto);
+        }
+
+        public decimal Total()
+        {
+            decimal total = 0;
+
+            foreach (Producto producto in productos)
+            {
+                total += producto.valor;
+            }
+
+            return total;
+
+        }
+    }
+}
